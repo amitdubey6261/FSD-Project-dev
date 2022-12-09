a331a86b9fd8646fd68071c0c844f101f71b9b16
amitdubey <amitdubey6261@gmail.com>
2022-12-09 19:22:01 +0530

    final

---
+++ sockets/public/sketch.js
@@ -0,0 +1,34 @@
+var socket;
+
+function setup() {
+  createCanvas(window.screen.availWidth, window.screen.availHeight);
+  background(0);
+  socket = io.connect('http://localhost:3000');
+  socket.on('mouse',
+    function(data) {
+      console.log("Got: " + data.x + " " + data.y);
+      fill(0,0,255);
+      noStroke();
+      ellipse(data.x, data.y, 20, 20);
+    }
+  );
+}
+
+function draw() {
+}
+
+function mouseDragged() {
+  fill(255);
+  noStroke();
+  ellipse(mouseX,mouseY,20,20);
+  sendmouse(mouseX,mouseY);
+}
+
+function sendmouse(xpos, ypos) {
+  console.log("sendmouse: " + xpos + " " + ypos);
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+  socket.emit('mouse',data);
+}
